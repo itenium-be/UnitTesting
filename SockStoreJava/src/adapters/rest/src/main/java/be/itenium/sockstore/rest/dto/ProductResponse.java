19aaaffe4cfde65b300f8733c63ecbc85dafa136
Michael Branders <michael.branders@benerail.com>
2025-06-19 19:26:06 +0200

    java sockstore unittesting

--- SockStoreJava/src/adapters/rest/src/main/java/be/itenium/sockstore/rest/dto/ProductResponse.java
+++ SockStoreJava/src/adapters/rest/src/main/java/be/itenium/sockstore/rest/dto/ProductResponse.java
@@ -5,10 +5,10 @@ import be.itenium.sockstore.vocabulary.Product;
 public record ProductResponse(String id, String naam, String categorie, Double prijs, int voorraad) {
     public static ProductResponse toRest(Product product) {
         return new ProductResponse(
-                product.id().toString(),
-                product.naam(),
-                product.categorie(),
-                product.prijs().doubleValue(),
+                product.id().value(),
+                product.naam().value(),
+                product.categorie().value(),
+                product.prijs().value().doubleValue(),
                 product.voorraad()
         );
     }
