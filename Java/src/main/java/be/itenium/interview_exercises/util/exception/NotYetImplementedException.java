7c467d9f2d5cd627d96ddd5e646f1b8696dbe64c
Wouter Van Schandevijl <woutervs@hotmail.com>
2025-07-01 19:59:45 +0200

    delete old java files

--- Java/src/main/java/be/itenium/interview_exercises/util/exception/NotYetImplementedException.java
+++
@@ -1,9 +0,0 @@
-package be.itenium.interview_exercises.util.exception;
-
-public class NotYetImplementedException extends RuntimeException {
-
-	public NotYetImplementedException() {
-		super("Implement this method as requested.");
-	}
-
-}
--- Java/src/main/java/be/itenium/interview_exercises/util/model/Sex.java
+++
@@ -1,6 +0,0 @@
-package be.itenium.interview_exercises.util.model;
-
-public enum Sex {
-	MALE,
-	FEMALE,
-}
