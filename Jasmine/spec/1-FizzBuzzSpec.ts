9ac0349b984737e1d6b306e036965b4921cb928e
Laoujin <woutervs@hotmail.com>
2026-04-08 11:58:55 +0200

    delete old jasmine stuff

--- Jasmine/spec/1-FizzBuzzSpec.ts
+++
@@ -1,8 +0,0 @@
-import FizzBuzz from '../1-FizzBuzz';
-
-describe('FizzBuzz', () => {
-  it('returns 1 as the first in the sequence', () => {
-
-
-  });
-});
--- Jasmine/spec/5-MazeSpec.ts
+++
@@ -1,7 +0,0 @@
-import {Maze, Crossroad} from '../5-Maze';
-
-describe('The Maze', () => {
-  it('can solve a simple maze', () => {
-
-  });
-});
